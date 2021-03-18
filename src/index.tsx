cccf7ad496178b1e87d766114e9af7b4fa2b6f60
Oliver Kramer <3520_kramer@live.dk>
2021-03-18 01:14:05 +0100

    deleted old css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home'
