5977d5139329d9c9cb5a98d5cb9122a40a86ee95
MadsRF <madsrunefrederiksen@gmail.com>
2021-03-26 09:44:24 +0100

    starting on geolocation

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home'
