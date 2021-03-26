bf9898f9e29799e3830d76474712c42211ed7279
Mads R. Frederiksen <madsrunefrederiksen@gmail.com>
2021-03-26 14:59:11 +0100

    map and geolocation working. missing styling and logic for accuracy
    * add geo folder structure
    
    * starting on geolocation
    
    * working on geolocation
    
    * map and geolocation working. missing styling and logic for accuracy

--- src/components/Teacher/RegisterAttendance/RegisterAttendance.tsx
+++ src/components/Teacher/RegisterAttendance/RegisterAttendance.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import VerticalStepper from '../../Common/VerticalStepper/VerticalStepper'
 import CoursesAndClasses from './CoursesAndClasses/CoursesAndClasses';
 import GenerateCode from './GenerateCode/GenerateCode';
+import Map from '../../Common/Map/Map'
 import './RegisterAttendance.css'
+import { Coordinates, getGeoLocation } from '../../../services/GeoService';
 
 export const RegisterAttendance = () => {
+    const [location, setLocation] = useState<Coordinates>({latitude: 0, longitude: 0, accuracy: 0});
+
+    useEffect(() => {
+        getGeoLocation();
+    },[])
+
+    useEffect(() => {
+        console.log('location', location);    
+    },[location])
+
+    const getGeoLocation = () => {
+        navigator.geolocation.getCurrentPosition( (position) => {
+      
+            const coordinates: Coordinates = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              accuracy: position.coords.accuracy,
+            }
+            
+            setLocation(coordinates);
+          },
+          (error => {
+            console.error("Error Code = " + error.code + " - " + error.message);
+          }), {enableHighAccuracy: true}
+        );
+      }
+
+
     return (
         <>
             <VerticalStepper>
                 <CoursesAndClasses />
                 <GenerateCode />
+                <Map long={location.longitude} lang={location.latitude}/>
                 {/*
                 The geolocation component
                 Possibly with this toggleswitch
