5d2d2f3b2529ea01f0efdbd9957f27c8d2797a70
MadsRF <madsrunefrederiksen@gmail.com>
2021-03-26 10:45:59 +0100

    working on geolocation

--- src/components/Geo/Geo.tsx
+++ src/components/Geo/Geo.tsx
@@ -1,1 +1,31 @@
-import React, { useState } from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        console.log(position);
+      },
+      function(error) {
+        console.error("Error Code = " + error.code + " - " + error.message);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Using geolocation JavaScript API in React</h4>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
