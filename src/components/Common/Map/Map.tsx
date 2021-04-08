0ddc34ce0f778f92d89fc98fb7576e154503c45c
MadsRF <madsrunefrederiksen@gmail.com>
2021-04-08 13:42:41 +0200

    merge

--- src/components/Common/Map/Map.tsx
+++ src/components/Common/Map/Map.tsx
@@ -6,7 +6,7 @@ const Marker = ({text}: any) => <div>{text}</div>;
 // TODO: needs to figure out how to get local location (lang/long) from Geo 
 const Map = (props: any) => {
     const [center, setCenter] = useState({lat: props.lang, lng:  props.long});
-    const [zoom, setZoom] = useState(11);
+    const [zoom, setZoom] = useState(17);
     return (
         <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
