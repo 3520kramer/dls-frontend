7fd9fa96f7be5502f45aa04b7a4caa5d19de8644
3520kramer <43636461+3520kramer@users.noreply.github.com>
2021-04-13 13:48:26 +0200

    Kramer/config vars (#12)
    * added config vars, added config vars to gitignore, added routes based on env
    
    * google api key is now dependant on env
    
    * added .env to gitignore

--- src/components/Common/Map/Map.tsx
+++ src/components/Common/Map/Map.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import "./Map.css"
 import LoadingOverlay from 'react-loading-overlay-ts';
-import { GOOGLE_API_KEY } from '../../../configuration/ConfigurationVariables';
 
 interface IProps{
   children?: React.ReactNode,
@@ -24,10 +23,12 @@ const Marker = (props: any) => {
 
 
 const Map: React.FC<IProps> = ({latitude, longitude, hasEnabledGPS}) => {
-  const [isActive, setActive] = useState(false)
+    const [isActive, setActive] = useState(false)
     const [center, setCenter] = useState({lat: latitude, lng: longitude});
     const [zoom, setZoom] = useState(17);
-
+    
+    const GOOGLE_API_KEY: string = process.env.GOOGLE_API_KEY !== undefined ? process.env.GOOGLE_API_KEY : "";
+    
     // for debugging
     useEffect(() => {
       console.log("hasEnabledGPS", hasEnabledGPS);
