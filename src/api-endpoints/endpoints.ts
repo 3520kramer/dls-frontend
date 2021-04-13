2eea3bd94822c17989254ba9edcc4632fba7f3dd
Oliver Kramer <3520_kramer@live.dk>
2021-04-13 14:30:05 +0200

    added REACT_APP to routes

--- src/api-endpoints/endpoints.ts
+++ src/api-endpoints/endpoints.ts
@@ -1,7 +1,7 @@
-export const ROOT_URL = process.env.ROOT_URL || "localhost:port";
+export const ROOT_URL = process.env.REACT_APP_ROOT_URL || "localhost:port";
 
 export const MODULE_ROUTE = `${ROOT_URL}/modules`;
 
-export const COURSES_ROUTE = process.env.TEST_URL || "https://run.mocky.io/v3/3f8459f4-b181-4d9c-8a06-916eb831ea21";
+export const COURSES_ROUTE = process.env.REACT_APP_TEST_URL || "https://run.mocky.io/v3/3f8459f4-b181-4d9c-8a06-916eb831ea21";
 
 
