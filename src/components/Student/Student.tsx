e09858c951b0f6f4c757869ffa975488466be0f8
Aviater <benedict.marien@gmail.com>
2021-04-30 11:54:51 +0200

    Removed student component

--- src/components/Student/Student.tsx
+++ src/components/Student/Student.tsx
@@ -4,7 +4,7 @@ import './Student.css'
 // temp. - have to create for student
 import Tabs from '../Common/Tabs/Tabs';
 import RegisterAttendanceStudent from './RegisterAttendanceStudent/RegisterAttendanceStudent'
-import { AttendeeStatistics } from '../Teacher/AttendeeStatistics/AttendeeStatistics';
+// import AttendeeStatistics from '../Teacher/AttendeeStatistics/AttendeeStatistics';
 
 
 export const Student = () => {
@@ -13,7 +13,7 @@ export const Student = () => {
             <Tabs
                 components={[
                     { component: <RegisterAttendanceStudent />, label: "Student Code" },
-                    { component: <AttendeeStatistics />, label: "View Your Attendee Statistics" }
+                    // { component: <AttendeeStatistics />, label: "View Your Attendee Statistics" }
                 ]}
             />
         </>
