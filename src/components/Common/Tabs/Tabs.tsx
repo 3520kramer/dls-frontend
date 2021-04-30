29a47817e0f2c57aae1fa61d58998a73b1cffcae
Oliver Kramer <3520_kramer@live.dk>
2021-04-30 11:12:08 +0200

    made onTabChange in Tabs component optional

--- src/components/Common/Tabs/Tabs.tsx
+++ src/components/Common/Tabs/Tabs.tsx
@@ -30,7 +30,7 @@ interface IComponent{
 
 type Props = {
   components: IComponent[],
-  onTabChange: Function
+  onTabChange?: Function
 }
 
 export default function CenteredTabs({ components, onTabChange }: Props) {
@@ -39,7 +39,7 @@ export default function CenteredTabs({ components, onTabChange }: Props) {
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
-    onTabChange();
+    if(onTabChange !== undefined) onTabChange() ;
   };
 
   return (
