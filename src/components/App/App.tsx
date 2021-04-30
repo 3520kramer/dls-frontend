8ef2f9ca876777481ae6548359bf5f9f5cf2fdfd
3520kramer <43636461+3520kramer@users.noreply.github.com>
2021-04-30 17:48:46 +0200

    merge resolved (#24)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, useHistory, Switch } from 'react-router-dom';
+import React, { useState }from 'react';
+import { Route, useHistory, Switch, RouteComponentProps } from 'react-router-dom';
 import { OktaAuth } from '@okta/okta-auth-js';
 import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Container } from 'semantic-ui-react';
@@ -8,12 +8,15 @@ import CustomLoginComponent from '../Authentication/Login';
 import Navbar from '../Common/Navbar/Navbar';
 import { Home } from '../Home/Home'
 import { Teacher } from '../Teacher/Teacher';
+import Header from '../Common/Header/Header';
 
 const oktaAuth = new OktaAuth(config.oidc);
 
 
 function App() {
     const history = useHistory(); // example from react-router
+
+    const [activeCodes, setActiveCodes] = useState<number>(0);
 
   const customAuthHandler = () => {
     // Redirect to the /login page that has a CustomLoginComponent
@@ -28,10 +31,11 @@ function App() {
     <Navbar />
         <Container text style={{ marginTop: '7em' }}>
             <Switch>
+                <Route path="/:page" component={(props: RouteComponentProps) => <Header activeCodes={activeCodes} {...props}/> }/>
                 <Route path="/login/callback" component={LoginCallback} />
                 <Route path="/login" component={CustomLoginComponent} />
                 <SecureRoute exact path="/"> <Home/> </SecureRoute>
-                <SecureRoute exact path="/teacher"> <Teacher/> </SecureRoute>
+                <SecureRoute exact path="/teacher"> <Teacher activeCodesCallback={(value: number) => setActiveCodes(value)}/> </SecureRoute>
             </Switch>
         </Container>
     </Security>
