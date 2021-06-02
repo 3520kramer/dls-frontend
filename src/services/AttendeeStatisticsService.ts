2693d7738c0f73830652b0ba4662adf50e9d9e64
Mads R. Frederiksen <madsrunefrederiksen@gmail.com>
2021-06-02 11:21:55 +0200

    Kramer/fix wrong call to api (#36)
    * fixed wrong way of calling API
    
    * added comments
    
    Co-authored-by: Oliver Kramer <3520_kramer@live.dk>

--- src/services/AttendeeStatisticsService.ts
+++ src/services/AttendeeStatisticsService.ts
@@ -1,18 +1,20 @@
-import { ROOT_URL, requestHeader } from "../api-endpoints/endpoints";
+import { STATISTICS_ROUTE, requestHeader } from "../api-endpoints/endpoints";
 
 export const fetchUserData = async (accessToken: string, subjectsSelected: Array<object>, classesSelected: Array<object>) => {
-    const subjectQueryArray = subjectsSelected.map((subject: any) => {
-        const str = subject.title.replaceAll(' ', '%20');
-        return `subject=${str}`;
-    });
 
-    const classQueryArray = classesSelected.map((cls: any) => {
-        const str = cls.title.replaceAll(' ', '%20');
-        return `class=${str}`;
-    });
+    let url = new URL(STATISTICS_ROUTE);
 
-    const studentsUrl = `${ROOT_URL}/api/statistics?${subjectQueryArray.toString()}&${classQueryArray.toString()}`;
-    const response = await fetch(studentsUrl, {
+    // add the subjects as search params
+    subjectsSelected.forEach((subject: any) => {
+        url.searchParams.append("subject", subject.title)
+    });
+    
+    // add the classes as search params
+    classesSelected.forEach((oneClass: any) => {
+        url.searchParams.append("class", oneClass.title)
+    });
+    
+    const response = await fetch(url.href, {
         method: 'GET',
         headers: requestHeader('GET', accessToken)
         }).then((async res => {
@@ -20,6 +22,5 @@ export const fetchUserData = async (accessToken: string, subjectsSelected: Array
             return data;
         })
     );
-
     return response;
 } 
